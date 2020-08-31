b1efcc9ccc8da44da826495f3715907918a5c37f
Jason Yalim <yalim@asu.edu>
2020-08-31 13:09:16 -0700

    init

---
+++ intro-python/stats/gen_data.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Used to generate the data for the statistics exercise
+##
+
+gen_data() {
+  seed="$1"
+  python << __EOF
+import numpy as np
+seed = $seed
+np.random.seed(seed=seed)
+N = 501
+x = 2*np.pi*np.linspace(0,1,N)[:-1]
+y = np.sin(x)
+z = np.random.normal(0,0.1,len(y))
+np.savetxt(f"dat/seed_{seed:03d}.txt",np.c_[x,y+z],header='',comments='')
+__EOF
+  echo $seed
+}
+
+! [[ -d dat/ ]] && mkdir dat/ || :
+
+export -f gen_data
+
+N=$(nproc)
+parallel -kvj$N gen_data ::: $(seq 0 999)
