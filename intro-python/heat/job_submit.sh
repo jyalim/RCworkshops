1d0689eded59c2740b92239e71fe8b444ef2e1a1
Jason Yalim <yalim@asu.edu>
2021-08-02 12:47:38 -0700

    heat

---
+++ intro-python/heat/job_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -p htc
+#SBATCH -q normal
+#SBATCH -t 5
+#SBATCH -c 2
+#SBATCH -o slurm.%A.%a.%j.out
+#SBATCH --array=1-4
+#SBATCH --export=NONE
+
+module load anaconda/py3
+source activate pyintel
+
+python heatnb.py $SLURM_ARRAY_TASK_ID
