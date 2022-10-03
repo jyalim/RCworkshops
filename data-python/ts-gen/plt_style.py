9663ef1eebf31c57fa873e20cdda719b5161daf6
Jason Yalim <yalim@asu.edu>
2022-10-03 14:17:26 -0700

    init

---
+++ data-python/ts-gen/plt_style.py
@@ -0,0 +1,37 @@
+from matplotlib import rcParams
+
+gld = '#FFC627'
+mrn = '#8C1D40'
+dpu = '#0C0C12'
+
+rcParams.update({
+  'axes.facecolor'       : '#111111',
+  'axes.edgecolor'       : 'w',
+  'figure.facecolor'     : '#111111',
+  'xtick.color'          : 'w',
+  'ytick.color'          : 'w',
+  'text.color'           : 'w',
+  'font.size'            : 20,
+  'axes.labelsize'       : 20,
+  'axes.titlesize'       : 24,
+  'xtick.major.pad'      : 10,
+  'xtick.minor.pad'      : 10,
+  'xtick.major.size'     : 8,
+  'xtick.minor.size'     : 4,
+  'xtick.major.width'    : 1.25,
+  'xtick.minor.width'    : 1.25,
+  'xtick.minor.visible'  : True,
+  'xtick.direction'      : 'in',
+  'xtick.bottom'         : True,
+  'xtick.top'            : True,
+  'ytick.major.pad'      : 10,
+  'ytick.minor.pad'      : 10,
+  'ytick.major.size'     : 8,
+  'ytick.minor.size'     : 4,
+  'ytick.major.width'    : 1.25,
+  'ytick.minor.width'    : 1.25,
+  'ytick.minor.visible'  : True,
+  'ytick.direction'      : 'in',
+  'ytick.left'           : True,
+  'ytick.right'          : True,
+})
---
+++ data-python/ts-gen/ts_helper.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pylab as plt
+
+def reader(f,offset=256,dtype=np.double,skip=19):
+
