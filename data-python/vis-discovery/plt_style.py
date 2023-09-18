5a8556b9eacb011c77f38ade4060dc77c8e4816a
[ Jason Yalim - RTO Scientific Software Engrng - Faculty - jyalim ] <jyalim@dg051.sol.rc.asu.edu>
2023-09-18 10:44:09 -0700

    init

---
+++ data-python/vis-discovery/plt_style.py
@@ -0,0 +1,41 @@
+from matplotlib import rcParams
+
+gld = '#FFC627'
+mrn = '#8C1D40'
+dpu = '#0C0C12'
+
+rcParams['figure.figsize']      = (10,6)
+rcParams['axes.facecolor']      = 'w'
+rcParams['axes.edgecolor']      = 'k'
+rcParams['figure.facecolor']    = 'w'
+rcParams['xtick.color']         = 'k'
+rcParams['ytick.color']         = 'k'
+rcParams['text.color']          = 'k'
+rcParams['font.size']           = 20
+rcParams['axes.labelsize']      = 20
+rcParams['axes.titlesize']      = 24
+rcParams['legend.fontsize']     = 14
+rcParams['xtick.direction']     = 'in'
+rcParams['ytick.direction']     = 'in'
+rcParams['xtick.major.pad']     = 10
+rcParams['xtick.minor.pad']     = 10
+rcParams['xtick.major.size']    = 8
+rcParams['xtick.minor.size']    = 4
+rcParams['xtick.major.width']   = 1.25
+rcParams['xtick.minor.width']   = 1.25
+rcParams['xtick.minor.visible'] = True
+rcParams['xtick.direction']     = 'in'
+rcParams['xtick.bottom']        = True
+rcParams['xtick.top']           = True
+rcParams['ytick.major.pad']     = 10
+rcParams['ytick.minor.pad']     = 10
+rcParams['ytick.major.size']    = 8
+rcParams['ytick.minor.size']    = 4
+rcParams['ytick.major.width']   = 1.25
+rcParams['ytick.minor.width']   = 1.25
+rcParams['ytick.minor.visible'] = True
+rcParams['ytick.direction']     = 'in'
+rcParams['ytick.left']          = True
+rcParams['ytick.right']         = True
+
+
